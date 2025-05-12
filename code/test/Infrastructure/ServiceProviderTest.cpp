07e4704333e27b7dfda0bac7ac40aae37c868f6d
hadan <nguyenhaidang911@gmail.com>
2025-05-13 04:26:38 +0700

    add full test

--- code/test/Infrastructure/ServiceProviderTest.cpp
+++ code/test/Infrastructure/ServiceProviderTest.cpp
@@ -30,8 +30,41 @@ public:
     }
 };
 
+class IService
+{
+public:
+    virtual void method() = 0;
+};
+
+class Service : public IService
+{
+public:
+    void method() override
+    {
+    }
+};
+
 TEST_F(ServiceProviderTest, testCreation)
 {
     auto serviceProvider = ServiceProvider::getInstance();
     EXPECT_NE(serviceProvider, nullptr);
+}
+
+TEST_F(ServiceProviderTest, testServiceRegistration)
+{
+    auto serviceProvider = ServiceProvider::getInstance();
+    EXPECT_NE(serviceProvider, nullptr);
+
+    EXPECT_FALSE(serviceProvider->hasService<IService>());
+
+    auto service1 = std::make_shared<Service>();
+    EXPECT_TRUE(serviceProvider->registerService<IService>(service1));
+    EXPECT_EQ(service1, serviceProvider->getService<IService>());
+
+    EXPECT_FALSE(serviceProvider->registerService<IService>(service1));
+
+    auto service2 = std::make_shared<Service>();
+    EXPECT_TRUE(serviceProvider->registerService<IService>(service2, true));
+    EXPECT_NE(service1, serviceProvider->getService<IService>());
+    EXPECT_EQ(service2, serviceProvider->getService<IService>());
 }
